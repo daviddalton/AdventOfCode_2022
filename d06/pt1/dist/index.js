5940cb0b85190c162660c2c5ba69b5d3144a63ff
David Dalton <david@chaddalton.com>
2022-12-06 03:09:50 -0500

    Finish  - pt1

--- d06/pt1/dist/index.js
+++ d06/pt1/dist/index.js
@@ -26,5 +26,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data = rawData.split('\n');
+for (let i = 3; i < rawData.length; i++) {
+    let temp = [];
+    temp.push(rawData[i], rawData[i - 1], rawData[i - 2], rawData[i - 3], rawData[i - 4], rawData[i - 5], rawData[i - 6], rawData[i - 7], rawData[i - 8], rawData[i - 9], rawData[i - 10], rawData[i - 11], rawData[i - 12], rawData[i - 13]);
+    console.log(temp);
+    if (new Set(temp).size == temp.length) {
+        console.log(i + 1);
+        break;
+    }
+}
 //# sourceMappingURL=index.js.map
