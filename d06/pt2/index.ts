66fe8ea8866558925a715c47708bd473a8522dc6
David Dalton <david@chaddalton.com>
2022-12-06 03:10:21 -0500

    Finish  - pt2

--- d06/pt2/index.ts
+++ d06/pt2/index.ts
@@ -5,10 +5,7 @@ const rawData: string = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 
 for (let i = 3; i < rawData.length; i++) {
     let temp: string[] = []
-    temp.push(rawData[i])
-    temp.push(rawData[i - 1])
-    temp.push(rawData[i - 2])
-    temp.push(rawData[i - 3])
+    temp.push(rawData[i], rawData[i - 1], rawData[i - 2], rawData[i - 3], rawData[i - 4], rawData[i - 5], rawData[i - 6], rawData[i - 7], rawData[i - 8], rawData[i - 9], rawData[i - 10], rawData[i - 11], rawData[i - 12], rawData[i - 13])
 
     console.log(temp)
     if (new Set(temp).size == temp.length) {
