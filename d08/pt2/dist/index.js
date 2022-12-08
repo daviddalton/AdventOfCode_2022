a27d0977b47445007e710ac0cead8752ffcc4e16
David Dalton <david@chaddalton.com>
2022-12-08 01:45:32 -0500

    Finish  - pt2

--- d08/pt2/dist/index.js
+++ d08/pt2/dist/index.js
@@ -27,48 +27,85 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
-let totalVisible = (data.length * 2) + (data[0].length * 2) - 4;
+let bestVisibilityScore = 0;
 for (let i = 1; i < data.length - 1; i++) {
     let row = data[i];
     row.split("").forEach((tree, index) => {
         if (index != 0 && index != row.length - 1) {
             //move to the left until you are out
-            let tallestLeft = true;
+            let leftVisibility = 0;
             for (let j = index; j > 0; j--) {
-                if (tree <= row[j - 1]) {
-                    tallestLeft = false;
+                if (tree >= row[j - 1]) {
+                    if (tree == row[j - 1]) {
+                        leftVisibility++;
+                        break;
+                    }
+                    else {
+                        leftVisibility++;
+                    }
+                }
+                else {
+                    leftVisibility++;
                     break;
                 }
             }
             //move to the right until you are out
-            let tallestRight = true;
+            let rightVisibility = 0;
             for (let j = index; j < row.length; j++) {
-                if (tree <= row[j + 1]) {
-                    tallestRight = false;
+                if (tree >= row[j + 1]) {
+                    if (tree == row[j + 1]) {
+                        rightVisibility++;
+                        break;
+                    }
+                    else {
+                        rightVisibility++;
+                    }
+                }
+                else {
+                    rightVisibility++;
                     break;
                 }
             }
-            let tallestUp = true;
+            let upVisibility = 0;
             for (let j = i; j > 0; j--) {
                 let itemAbove = data[j - 1].split("")[index];
-                if (tree <= itemAbove) {
-                    tallestUp = false;
+                if (tree >= itemAbove) {
+                    if (tree == itemAbove) {
+                        upVisibility++;
+                        break;
+                    }
+                    else {
+                        upVisibility++;
+                    }
+                }
+                else {
+                    upVisibility++;
                     break;
                 }
             }
-            let tallestDown = true;
+            let downVisibility = 0;
             for (let j = i; j < data.length - 1; j++) {
                 let itemBelow = data[j + 1].split("")[index];
-                if (tree <= itemBelow) {
-                    tallestDown = false;
+                if (tree >= itemBelow) {
+                    if (tree == itemBelow) {
+                        downVisibility++;
+                        break;
+                    }
+                    else {
+                        downVisibility++;
+                    }
+                }
+                else {
+                    downVisibility++;
                     break;
                 }
             }
-            if (tallestDown || tallestLeft || tallestRight || tallestUp) {
-                totalVisible++;
+            let visibilityScore = leftVisibility * rightVisibility * downVisibility * upVisibility;
+            if (visibilityScore > bestVisibilityScore) {
+                bestVisibilityScore = visibilityScore;
             }
         }
     });
 }
-console.log(totalVisible);
+console.log(bestVisibilityScore);
 //# sourceMappingURL=index.js.map
